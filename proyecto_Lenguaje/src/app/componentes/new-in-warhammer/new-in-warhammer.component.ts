ea0a4f9d0c73be3c79d30744ccc53109b68f97cf
alejandro <alejandro.penellalopez@cieep.com>
2025-04-26 14:37:06 +0200

    Creacion de páginas de comunidad

---
+++ proyecto_Lenguaje/src/app/componentes/new-in-warhammer/new-in-warhammer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-in-warhammer',
+  imports: [],
+  templateUrl: './new-in-warhammer.component.html',
+  styleUrl: './new-in-warhammer.component.css'
+})
+export class NewInWarhammerComponent {
+
+}
