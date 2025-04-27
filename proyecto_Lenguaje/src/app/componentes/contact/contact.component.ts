98579991d7ee9dc29b671385ac6b2ef15cd6cc68
alejandro <alejandro.penellalopez@cieep.com>
2025-04-27 11:47:25 +0200

    cambios en página de productos y añadir top bar

--- proyecto_Lenguaje/src/app/componentes/contact/contact.component.ts
+++ proyecto_Lenguaje/src/app/componentes/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TopBarComponent } from '../top-bar/top-bar.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [TopBarComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
